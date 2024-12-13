e7136c5975e39d0ba3df980451b289a1cdaa6f10
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-12-13 15:07:37 +0100

    Add excalidraw pic to the project and fetch the art pieces in console

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
+
+  const {data, isLoading, error} = useSWR("https://example-apis.vercel.app/api/art",fetcher)
+  console.log("art pieces: ", data)
+
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
+
   return (
     <>
       <GlobalStyle />
