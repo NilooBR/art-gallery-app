7cc7a7f91de3eca9341c0f8ed3f4fad7510e7635
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2024-12-16 16:07:07 +0100

    add spotlight pgae

--- pages/Spotlight/index.js
+++ pages/Spotlight/index.js
@@ -1,9 +1,61 @@
+import ArtPiece from "@/components/ArtPiece";
+import FavoriteButton from "@/components/FavoriteButton";
 import Navigation from "@/components/Navigation";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
-export default function Spotlight() {
+const StyledHeader = styled.h1`
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row;
+`;
+const StyledPageTitle = styled.h2`
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row;
+`;
+
+export default function Spotlight({
+  artPieces,
+  favoriteArt,
+  onToggleFavorite,
+}) {
+  const [spotlight, setSpotlight] = useState(null);
+
+  useEffect(() => {
+    setSpotlight(artPieces[Math.floor(Math.random() * artPieces.length)]);
+  }, [artPieces]);
+
+  const isFavorite =
+    (Array.isArray(favoriteArt) &&
+      favoriteArt.find((art) => art.slug === artPieces.slug)
+        ?.isFavorite) ||
+    false;
+
+    if(!spotlight){
+      return <div>Loading Spotlight</div>
+    }
+
   return (
     <>
-      <p>is Coming...</p>
+      <StyledHeader>Art Gallery</StyledHeader>
+
+      <StyledPageTitle>Spotlight</StyledPageTitle>
+
+      <FavoriteButton
+        isFavorite={isFavorite}
+        onToggleFavorite={() => onToggleFavorite(setSpotlight.slug)}
+      ></FavoriteButton>
+
+      <br></br>
+
+      <ArtPiece
+        name={spotlight.name}
+        imageSource={spotlight.imageSource}
+        artist={spotlight.artist}
+        year={spotlight.year}
+        genre={spotlight.genre}
+      ></ArtPiece>
       <Navigation></Navigation>
     </>
   );
